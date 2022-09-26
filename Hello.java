e347e46b215c59d53254443e4eff3f1e7586e776
Park minsu <cmwmss54@gmail.com>
2022-09-26 16:58:35 +0900

    merge 커밋

--- Hello.java
+++ Hello.java
@@ -3,6 +3,10 @@ public class Hello{
     String msg="hello";
     public void say()
     {
+        say(this.msg);
+    }
+    public void say(String msg)
+    {
         System.out.println(msg);
     }
 }
